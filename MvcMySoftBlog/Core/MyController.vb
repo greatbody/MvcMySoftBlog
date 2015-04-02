f9b7313e6641f5192b891fbd0a58f671b72d60b5
greatbody <wedown@163.com>
2015-04-02 20:23:49 +0800

    update controller

--- MvcMySoftBlog/Core/MyController.vb
+++ MvcMySoftBlog/Core/MyController.vb
@@ -11,13 +11,33 @@
                 _user = value
             End Set
         End Property
+        Public Sub New()
+            '无参数构造函数
+        End Sub
+
+        Public Sub New(ByVal requestContext As RequestContext)
+            Me.OnInit(requestContext)
+        End Sub
 
         Public Overridable Sub OnInit()
-            If Me.HttpContext.Session("UserLog") Is Nothing Then
-            Else
+            If Not Me.HttpContext.Session("UserLog") Is Nothing Then
                 BlogUser = Me.HttpContext.Session("UserLog")
+                Dim ControllerName As String = Me.RouteData.Values("controller").ToString() + "Controller"
+                Dim ActionName As String = Me.RouteData.Values("action").ToString()
+            Else
+                '无权限的用户跳转到主页面
+                Me.HttpContext.Response.Redirect("/")
             End If
         End Sub
+
+        Public Overridable Sub OnInit(ByVal requestContext As System.Web.Routing.RequestContext)
+            If Not requestContext.HttpContext.Session("UserLog") Is Nothing Then
+                BlogUser = requestContext.HttpContext.Session("UserLog")
+                Dim ControllerName As String = requestContext.RouteData.Values("controller").ToString() + "Controller"
+                Dim ActionName As String = requestContext.RouteData.Values("action").ToString()
+            End If
+        End Sub
+
     End Class
 End Namespace
 
